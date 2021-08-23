8c237654b8955aa5e2517d43dfcdab4cd740128f
Liz Wilson <lizriderwilson@gmail.com>
2021-08-23 16:24:51 -0400

    commit tree 5f0847e7054ee8a7538d97864df3570efdadb047

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import TwitterMessage from "./components/TwitterMessage";
 
 class App extends Component {
 
+
   login = ({ username, password }) => {
     console.log(`Logging in ${username} with password ${password}`);
   };
@@ -20,7 +21,7 @@ class App extends Component {
         <h1>
           <pre>TwitterMessage</pre>
         </h1>
-        <TwitterMessage maxChars={280} />
+        <TwitterMessage maxChars={280}/>
 
 
 
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -3,15 +3,29 @@ import React from "react";
 class TwitterMessage extends React.Component {
   constructor() {
     super();
-
-    this.state = {};
+    this.state = {
+      message: ''
+    }
   }
 
+  handleMessageChange = (event) => {
+    this.setState({
+      message: event.target.value
+    });
+  };
+
   render() {
+    let charNumber = this.props.maxChars - this.state.message.length;
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input
+          type="text"
+          name="message"
+          id="message"
+          onChange={this.handleMessageChange}
+          value={this.state.message} />
+          {charNumber}
       </div>
     );
   }
